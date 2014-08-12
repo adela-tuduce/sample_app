e9808e2ec6105e4eceacfbe9fbb161aeb7a3c34d
Adela <adela_maria13@yahoo.com>
2014-08-12 08:11:11 +0300

    Add a future class

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,6 +1,7 @@
 require 'spec_helper'
 
 describe "Static pages" do
+  let(:base_title){"Ruby"}
 
   describe "Home page" do
 
@@ -10,7 +11,11 @@ describe "Static pages" do
     end
     it "should have the title 'Home'" do
       visit '/static_pages/home'
-      expect(page).to have_title('Ruby|Home')
+      expect(page).not_to have_title("|Home")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby")
     end
   end
 
@@ -20,9 +25,9 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help Page')
     end
-     it "should have the title 'Home'" do
+     it "should have the title 'Help'" do
       visit '/static_pages/help'
-      expect(page).to have_title('Ruby|Help')
+      expect(page).to have_title("#{base_title}|Help")
     end
   end
   
@@ -33,7 +38,7 @@ describe "Static pages" do
     end
      it "should have the title 'Home'" do
       visit '/static_pages/about'
-      expect(page).to have_title('Ruby|About')
+      expect(page).to have_title("#{base_title}|About")
     end
   end
 
@@ -44,7 +49,7 @@ describe "Static pages" do
     end
     it "Should have the title 'Contact'" do
       visit '/static_pages/contact'
-      expect(page).to have_title('Ruby|Contact')
+      expect(page).to have_title("#{base_title}|Contact")
     end
   end
 end
